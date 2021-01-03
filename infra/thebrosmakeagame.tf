6b636f2fc933eaf89b995c303b8f8d1e9db82333
theandrew168 <steveday168@gmail.com>
2021-01-03 12:07:34 -0600

    Update thebrosmakeagame terraform config

--- infra/thebrosmakeagame.tf
+++ infra/thebrosmakeagame.tf
@@ -10,8 +10,8 @@ resource "digitalocean_app" "thebrosmakeagame" {
       instance_count     = 1
       instance_size_slug = "basic-xxs"
 
-      run_command = "python3 app.py"
-      http_port   = 8080
+      run_command = "python3 main.py"
+      http_port   = 8000
 
       github {
         repo   = "theandrew168/thebrosmakeagame"
