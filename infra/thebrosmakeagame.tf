e806dd5fbb2d84a6e2f26ae57da4b2be0c7fa3ef
theandrew168 <steveday168@gmail.com>
2021-01-02 19:14:38 -0600

    Add game site as a DO app

---
+++ infra/thebrosmakeagame.tf
@@ -0,0 +1,28 @@
+resource "digitalocean_app" "thebrosmakeagame" {
+  spec {
+    name    = "thebrosmakeagame"
+    region  = "nyc"
+    domains = ["thebrosmakeagame.com"]
+
+    service {
+      name               = "thebrosmakeagame"
+      environment_slug   = "python"
+      instance_count     = 1
+      instance_size_slug = "basic-xxs"
+
+      run_command = "python3 app.py"
+      http_port   = 8080
+
+      github {
+        repo   = "theandrew168/thebrosmakeagame"
+        branch = "main"
+
+        deploy_on_push = true
+      }
+
+      routes {
+        path = "/"
+      }
+    }
+  }
+}
