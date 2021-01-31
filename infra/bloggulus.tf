d1e97c7607a559a0ab2afb0da95fb273c2dc80f3
theandrew168 <steveday168@gmail.com>
2021-01-30 23:38:01 -0600

    Add CNAME record for www.bloggulus.com

--- infra/bloggulus.tf
+++ infra/bloggulus.tf
@@ -14,12 +14,20 @@ resource "digitalocean_domain" "bloggulus" {
   ip_address = digitalocean_droplet.bloggulus.ipv4_address
 }
 
+resource "digitalocean_record" "bloggulus_cname_www" {
+  domain = digitalocean_domain.bloggulus.name
+  type   = "CNAME"
+  name   = "www"
+  value  = "@"
+  ttl    = "43200"
+}
+
 resource "digitalocean_record" "bloggulus_caa_letsencrypt" {
   domain = digitalocean_domain.bloggulus.name
-  name   = "@"
   type   = "CAA"
+  name   = "@"
   value  = "letsencrypt.org."
+  ttl    = "3600"
   flags  = "0"
   tag    = "issue"
-  ttl    = "3600"
 }
