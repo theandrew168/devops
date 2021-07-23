705dda858c533f886667e64c48608a0737de8d02
theandrew168 <andrew@shallowbrooksoftware.com>
2021-07-23 14:10:05 -0500

    Add DO Spaces backend and delete tfstate from repo

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,4 +1,14 @@
 terraform {
+  backend "s3" {
+    region   = "nyc3"
+    bucket   = "sbs-terraform"
+    key      = "terraform.tfstate"
+    endpoint = "nyc3.digitaloceanspaces.com"
+
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_metadata_api_check     = true
+  }
   required_providers {
     digitalocean = {
       source = "digitalocean/digitalocean"
