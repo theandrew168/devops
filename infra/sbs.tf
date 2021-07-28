49bd3f64aa8ef2a14edf10fdd7393e1e10a1dfbf
theandrew168 <andrew@shallowbrooksoftware.com>
2021-07-28 17:25:03 -0500

    Add SendGrid CNAMEs

--- infra/sbs.tf
+++ infra/sbs.tf
@@ -78,3 +78,43 @@ resource "digitalocean_record" "sbs_google_mx_alt4" {
   priority = 10
   ttl      = "3600"
 }
+
+resource "digitalocean_record" "sbs_sendgrid_cname1" {
+  domain = digitalocean_domain.sbs.name
+  type   = "CNAME"
+  name   = "url7023"
+  value  = "sendgrid.net."
+  ttl    = "43200"
+}
+
+resource "digitalocean_record" "sbs_sendgrid_cname2" {
+  domain = digitalocean_domain.sbs.name
+  type   = "CNAME"
+  name   = "22828247"
+  value  = "sendgrid.net."
+  ttl    = "43200"
+}
+
+resource "digitalocean_record" "sbs_sendgrid_cname3" {
+  domain = digitalocean_domain.sbs.name
+  type   = "CNAME"
+  name   = "em8428"
+  value  = "u22828247.wl248.sendgrid.net."
+  ttl    = "43200"
+}
+
+resource "digitalocean_record" "sbs_sendgrid_cname4" {
+  domain = digitalocean_domain.sbs.name
+  type   = "CNAME"
+  name   = "s1._domainkey"
+  value  = "s1.domainkey.u22828247.wl248.sendgrid.net."
+  ttl    = "43200"
+}
+
+resource "digitalocean_record" "sbs_sendgrid_cname5" {
+  domain = digitalocean_domain.sbs.name
+  type   = "CNAME"
+  name   = "s2._domainkey"
+  value  = "s2.domainkey.u22828247.wl248.sendgrid.net."
+  ttl    = "43200"
+}
