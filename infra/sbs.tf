c643890f93de08bf82517b91128ce80cdac7790a
theandrew168 <andrew@shallowbrooksoftware.com>
2021-07-28 16:45:59 -0500

    Add Google MX records for SBS

--- infra/sbs.tf
+++ infra/sbs.tf
@@ -33,3 +33,48 @@ resource "digitalocean_record" "sbs_cname_status" {
   value  = "stats.uptimerobot.com."
   ttl    = "43200"
 }
+
+resource "digitalocean_record" "sbs_google_mx" {
+  domain   = digitalocean_domain.sbs.name
+  type     = "MX"
+  name     = "@"
+  value    = "aspmx.l.google.com."
+  priority = 1
+  ttl      = "3600"
+}
+
+resource "digitalocean_record" "sbs_google_mx_alt1" {
+  domain   = digitalocean_domain.sbs.name
+  type     = "MX"
+  name     = "@"
+  value    = "alt1.aspmx.l.google.com."
+  priority = 5
+  ttl      = "3600"
+}
+
+resource "digitalocean_record" "sbs_google_mx_alt2" {
+  domain   = digitalocean_domain.sbs.name
+  type     = "MX"
+  name     = "@"
+  value    = "alt2.aspmx.l.google.com."
+  priority = 5
+  ttl      = "3600"
+}
+
+resource "digitalocean_record" "sbs_google_mx_alt3" {
+  domain   = digitalocean_domain.sbs.name
+  type     = "MX"
+  name     = "@"
+  value    = "alt3.aspmx.l.google.com."
+  priority = 10
+  ttl      = "3600"
+}
+
+resource "digitalocean_record" "sbs_google_mx_alt4" {
+  domain   = digitalocean_domain.sbs.name
+  type     = "MX"
+  name     = "@"
+  value    = "alt4.aspmx.l.google.com."
+  priority = 10
+  ttl      = "3600"
+}
