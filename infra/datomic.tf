a82820c5472f51ecdfd3c36f052f1811b622adff
theandrew168 <andrew@shallowbrooksoftware.com>
2021-08-02 18:49:57 -0500

    Add role and deployment for datomic

---
+++ infra/datomic.tf
@@ -0,0 +1,18 @@
+resource "digitalocean_droplet" "datomic" {
+  image    = "ubuntu-20-04-x64"
+  name     = "datomic"
+  region   = "nyc1"
+  size     = "s-2vcpu-4gb"
+  ssh_keys = [
+    digitalocean_ssh_key.andrew_macbook.fingerprint,
+    digitalocean_ssh_key.wesley_thinkpad.fingerprint,
+  ]
+}
+
+resource "digitalocean_record" "datomic_a" {
+  domain = digitalocean_domain.sandbox.name
+  type   = "A"
+  name   = "datomic"
+  value  = digitalocean_droplet.datomic.ipv4_address
+  ttl    = "3600"
+}
