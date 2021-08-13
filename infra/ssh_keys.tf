668aa0dd1071ecea4ebfece2ad6dafa98dd52b57
brueland <wesbrueland@gmail.com>
2021-08-13 13:36:32 -0500

    Removing old ssh keys

--- infra/ssh_keys.tf
+++ infra/ssh_keys.tf
@@ -3,11 +3,6 @@ resource "digitalocean_ssh_key" "andrew_macbook" {
   public_key = chomp(file("${path.module}/ssh_keys/andrew_macbook.pub"))
 }
 
-resource "digitalocean_ssh_key" "wesley_thinkpad" {
-  name       = "wesley_thinkpad"
-  public_key = chomp(file("${path.module}/ssh_keys/wesley_thinkpad.pub"))
-}
-
 resource "digitalocean_ssh_key" "wesley_klang" {
   name       = "wesley_klang"
   public_key = chomp(file("${path.module}/ssh_keys/wesley_klang.pub"))
