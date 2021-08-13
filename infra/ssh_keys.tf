a9d8bf542507b8c67edcecd152ca66e9e2cb5c11
brueland <wesbrueland@gmail.com>
2021-08-13 13:28:33 -0500

    Adding new ssh key for Klang

--- infra/ssh_keys.tf
+++ infra/ssh_keys.tf
@@ -7,3 +7,8 @@ resource "digitalocean_ssh_key" "wesley_thinkpad" {
   name       = "wesley_thinkpad"
   public_key = chomp(file("${path.module}/ssh_keys/wesley_thinkpad.pub"))
 }
+
+resource "digitalocean_ssh_key" "wesley_klang" {
+  name       = "wesley_klang"
+  public_key = chomp(file("${path.module}/ssh_keys/wesley_klang.pub"))
+}
