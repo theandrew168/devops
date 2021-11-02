3f90d425e95b539f5125bdc8ed6b2adf038091e4
theandrew168 <andrew@shallowbrooksoftware.com>
2021-11-01 21:58:22 -0500

    Add HEAD to allowed CORS methods

--- infra/cdn.tf
+++ infra/cdn.tf
@@ -5,7 +5,7 @@ resource "digitalocean_spaces_bucket" "sbs_cdn" {
 
   cors_rule {
     allowed_headers = ["*"]
-    allowed_methods = ["GET"]
+    allowed_methods = ["GET", "HEAD"]
     allowed_origins = ["*"]
     max_age_seconds = 3000
   }
