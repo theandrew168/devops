2c1ca38eb73ad898862fe28a4811dfc66004019b
theandrew168 <andrew@shallowbrooksoftware.com>
2021-12-26 21:21:12 -0600

    Remove infra for cortex, loki, and grafana

--- infra/cortex/main.tf
+++ infra/cortex/main.tf
@@ -3,34 +3,3 @@ resource "digitalocean_spaces_bucket" "cortex" {
   region = "nyc3"
   acl    = "private"
 }
-
-resource "digitalocean_droplet" "cortex" {
-  image  = "ubuntu-20-04-x64"
-  name   = "cortex"
-  region = "nyc1"
-  size   = "s-1vcpu-1gb"
-
-  ssh_keys = [
-    "9c:f4:8b:a5:4f:97:99:60:79:50:63:61:61:18:bc:d4",
-  ]
-}
-
-# hosted on a subdomain of shallowbrooksoftware.com
-
-resource "digitalocean_record" "cortex_a" {
-  domain = "shallowbrooksoftware.com"
-  type   = "A"
-  name   = "cortex"
-  value  = digitalocean_droplet.cortex.ipv4_address
-  ttl    = "3600"
-}
-
-resource "digitalocean_record" "cortex_caa_letsencrypt" {
-  domain = "shallowbrooksoftware.com"
-  type   = "CAA"
-  name   = "cortex"
-  value  = "letsencrypt.org."
-  ttl    = "3600"
-  flags  = "0"
-  tag    = "issue"
-}
--- infra/loki/main.tf
+++ infra/loki/main.tf
@@ -3,34 +3,3 @@ resource "digitalocean_spaces_bucket" "loki" {
   region = "nyc3"
   acl    = "private"
 }
-
-resource "digitalocean_droplet" "loki" {
-  image  = "ubuntu-20-04-x64"
-  name   = "loki"
-  region = "nyc1"
-  size   = "s-1vcpu-1gb"
-
-  ssh_keys = [
-    "9c:f4:8b:a5:4f:97:99:60:79:50:63:61:61:18:bc:d4",
-  ]
-}
-
-# hosted on a subdomain of shallowbrooksoftware.com
-
-resource "digitalocean_record" "loki_a" {
-  domain = "shallowbrooksoftware.com"
-  type   = "A"
-  name   = "loki"
-  value  = digitalocean_droplet.loki.ipv4_address
-  ttl    = "3600"
-}
-
-resource "digitalocean_record" "loki_caa_letsencrypt" {
-  domain = "shallowbrooksoftware.com"
-  type   = "CAA"
-  name   = "loki"
-  value  = "letsencrypt.org."
-  ttl    = "3600"
-  flags  = "0"
-  tag    = "issue"
-}
