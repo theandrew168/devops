066b6bb3da31d4c69ecd007a47965c3bb071131a
theandrew168 <andrew@shallowbrooksoftware.com>
2025-01-19 23:04:16 -0600

    Add server, volume, and DNS for derztunes

--- infra/derztunes/main.tf
+++ infra/derztunes/main.tf
@@ -3,3 +3,49 @@ resource "digitalocean_spaces_bucket" "derztunes" {
   region = "nyc3"
   acl    = "private"
 }
+
+resource "digitalocean_volume" "derztunes_db" {
+  name   = "derztunes-db"
+  region = "nyc1"
+  size   = 10
+
+  initial_filesystem_type = "ext4"
+}
+
+resource "digitalocean_droplet" "derztunes" {
+  image    = "ubuntu-24-04-x64"
+  name     = "derztunes"
+  region   = "nyc1"
+  size     = "s-1vcpu-1gb"
+
+  ssh_keys = [
+    "9c:f4:8b:a5:4f:97:99:60:79:50:63:61:61:18:bc:d4",
+  ]
+
+  volume_ids = [
+    digitalocean_volume.derztunes_db.id,
+  ]
+}
+
+resource "digitalocean_record" "derztunes_a" {
+  domain = "derztunes.com"
+  type   = "A"
+  name   = "@"
+  value  = digitalocean_droplet.derztunes.ipv4_address
+}
+
+resource "digitalocean_record" "derztunes_cname_www" {
+  domain = "derztunes.com"
+  type   = "CNAME"
+  name   = "www"
+  value  = "@"
+}
+
+resource "digitalocean_record" "derztunes_caa_letsencrypt" {
+  domain = "derztunes.com"
+  type   = "CAA"
+  name   = "@"
+  value  = "letsencrypt.org."
+  flags  = "0"
+  tag    = "issue"
+}
